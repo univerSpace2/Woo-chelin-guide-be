21176868f7d7b31c5624759987fd958ab9e97f69
universpace <woojoo1602@gmail.com>
2022-11-20 11:21:35 +0900

    add restaurants crud and review crud

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -1,15 +1,22 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ListSerializer
 
 from restaurants.models import Restaurant, Review
-
-
-class RestaurantSerializer(ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = '__all__'
+from accounts.serializers import UserSerializer
 
 
 class ReviewSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Review
         fields = '__all__'
+
+
+class RestaurantSerializer(ModelSerializer):
+    reviews = ListSerializer(child=ReviewSerializer(), read_only=True)
+    class Meta:
+        model = Restaurant
+        fields = (
+            'id', 'name', 'description', 'rating', 'average_price', 'address_ko', 'address_en', 'longitude', 'latitude',
+            'type', 'genre', 'reviews')
+        read_only_fields = ('id')
--- restaurants/views.py
+++ restaurants/views.py
@@ -2,13 +2,44 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
-from models import Restaurant
-from restaurants.serializers import RestaurantSerializer
+from .models import Restaurant, Review
+from restaurants.serializers import RestaurantSerializer, ReviewSerializer
 
 
 class RestaurantCRUD(ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            return [IsAuthenticated()]
+        else:
+            return [AllowAny()]
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+
+class ReviewCRUD(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get_permissions(self):
+        if self.action == 'create' or self.action == 'update' or self.action == 'destroy':
+            return [IsAuthenticated()]
+        return [AllowAny()]
 
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
